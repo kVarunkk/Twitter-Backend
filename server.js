b2edb9152fc6cd07c158b67c742497963769fad2
kVarunkk <varunkumawatleap2@gmail.com>
2023-03-11 18:48:42 +0530

    update server.js

--- server.js
+++ server.js
@@ -156,7 +156,9 @@ app.get("/feed/comments/:tweetId", (req, res) => {
 //compose tweet
 app.post("/feed", (req, res) => {
   const info = req.body;
-  const tweetInfo = req.body.tweet;
+  const tweetInfo = JSON.parse(req.body.tweet);
+
+  console.log(tweetInfo);
 
   newTweet = Tweet.create(
     {
