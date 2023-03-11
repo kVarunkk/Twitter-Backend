984a45f113eaeeea7875f19897a8eda1c8190ee5
kVarunkk <varunkumawatleap2@gmail.com>
2023-03-11 18:07:24 +0530

    test

--- server.js
+++ server.js
@@ -154,38 +154,6 @@ app.get("/feed/comments/:tweetId", (req, res) => {
 });
 
 //compose tweet
-app.post("/feed", (req, res) => {
-  const info = req.body;
-  const tweetInfo = req.body.tweet;
-
-  newTweet = Tweet.create(
-    {
-      content: tweetInfo.content,
-      retweets: [],
-      postedTweetTime: moment().format("MMMM Do YYYY, h:mm:ss a"),
-    },
-    (err, newTweet) => {
-      if (!err) {
-        if (info.image) {
-          newTweet.image = info.image;
-        } else console.log("no image found");
-        User.findOne({ username: tweetInfo.postedBy.username }, (err, doc) => {
-          if (!err) {
-            newTweet.postedBy = doc._id;
-            if (newTweet.postedBy) {
-              newTweet.save();
-              doc.tweets.unshift(newTweet._id);
-              doc.save();
-              return res.json({ image: info.image });
-            } else
-              return res.json({ status: "error", error: "An error occured" });
-          } else
-            return res.json({ status: "error", error: "An error occured" });
-        });
-      }
-    }
-  );
-});
 
 //compose comment
 app.post("/feed/comment/:tweetId", (req, res) => {
