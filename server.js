7e0c2c630fa5874e5afa2fbdc8957f733f15fdce
kVarunkk <varunkumawatleap2@gmail.com>
2023-04-09 00:07:41 +0530

    add build folder

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("build"));
 app.use("/images", express.static("images"));
 app.use("./tweetImages", express.static("tweetImages"));
 
