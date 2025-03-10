dbbe101559a4ddb6577fe5500dcd4eb23f29a639
kVarunkk <varunkumawatleap2@gmail.com>
2025-03-10 22:57:39 +0530

    chore: remove build folder

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const { v4: uuidv4 } = require("uuid");
 let path = require("path");
 require("dotenv").config();
 
-// const corsOptions = {
-//   origin: "*",
-//   optionSuccessStatus: 200,
-// };
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -586,6 +583,6 @@ app.get("/topic/:tag", async (req, res) => {
   }
 });
 
-app.listen("5000", () => {
-  console.log("server running on port 5000");
+app.listen(port, () => {
+  console.log("server running on port " + port);
 });
